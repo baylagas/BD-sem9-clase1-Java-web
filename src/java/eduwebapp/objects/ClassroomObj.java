51c1388af7c0c1c9efda4687debc6fa2c1e8088a
balbino aylagas <balbino_a@hotmail.com>
2020-07-22 08:52:07 -0600

    added classroomobj file

---
+++ src/java/eduwebapp/objects/ClassroomObj.java
@@ -0,0 +1,34 @@
+package eduwebapp.objects;
+
+public class ClassroomObj 
+{
+    private int id;
+    private String codigo;
+    private int capacidad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int pId) {
+        this.id = pId;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String pCodigo) {
+        this.codigo = pCodigo;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int pCapacidad) {
+        this.capacidad = pCapacidad;
+    }
+    
+    
+}
